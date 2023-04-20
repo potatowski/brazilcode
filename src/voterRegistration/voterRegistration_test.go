5839ee457466d2e41fdd93a33ef0d55829b26097
Vitor Rocha <deus.1928@gmail.com>
2023-04-20 11:45:29 -0400

    test: create test to function to format voter registration

--- src/voterRegistration/voterRegistration_test.go
+++ src/voterRegistration/voterRegistration_test.go
@@ -51,3 +51,56 @@ func TestIsValid(t *testing.T) {
 		})
 	}
 }
+
+func TestFormat(t *testing.T) {
+	tests := []struct {
+		name              string
+		voterRegistration string
+		expectedResult    string
+		expectedError     error
+	}{
+		{
+			name:              "valid voter registration",
+			voterRegistration: "356061030159",
+			expectedResult:    "3560 6103 0159",
+			expectedError:     nil,
+		},
+		{
+			name:              "voter registration with invalid length",
+			voterRegistration: "12345678901",
+			expectedResult:    "",
+			expectedError:     errors.New("Voter Registration with invalid length"),
+		},
+		{
+			name:              "voter registration with invalid UF",
+			voterRegistration: "356061032959",
+			expectedResult:    "",
+			expectedError:     errors.New("Invalid UF"),
+		},
+		{
+			name:              "voter registration with invalid check digit 1",
+			voterRegistration: "356061030119",
+			expectedResult:    "",
+			expectedError:     errors.New("Invalid Voter Registration"),
+		},
+		{
+			name:              "voter registration with invalid check digit 2",
+			voterRegistration: "356061030158",
+			expectedResult:    "",
+			expectedError:     errors.New("Invalid Voter Registration"),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result, err := Format(test.voterRegistration)
+			if err != nil && err.Error() != test.expectedError.Error() {
+				t.Errorf("Expected error %v but got %v", test.expectedError, err)
+			}
+
+			if result != test.expectedResult {
+				t.Errorf("Expected result %v but got %v", test.expectedResult, result)
+			}
+		})
+	}
+}
