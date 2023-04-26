9a5c84083b52531303ae6d79ce223233531fc69c
Vitor Rocha <deus.1928@gmail.com>
2023-04-25 20:02:51 -0400

    refactor: added description to functions in main file

--- brazilcode.go
+++ brazilcode.go
@@ -27,46 +27,105 @@ var (
 	ErrVoterRegistrationLimit         = voterRegistration.ErrVoterRegistrationLimit
 )
 
+/*
+CNPJIsValid check if the CNPJ is valid
+  - @param {string} doc - CNPJ
+  - @return {error} - error
+*/
 func CNPJIsValid(doc string) error {
 	return cnpj.IsValid(doc)
 }
 
+/*
+CNPJFormat format the an valid CNPJ
+  - @param {string} doc - CNPJ
+  - @return {string} - CNPJ formatted
+  - @return {error}	- error
+*/
 func CNPJFormat(doc string) (string, error) {
 	return cnpj.Format(doc)
 }
 
+/*
+CNPJGenerate generate a valid CNPJ
+  - @return {string} - CNPJ
+  - @return {error}	- error
+*/
 func CNPJGenerate() (string, error) {
 	return cnpj.Generate()
 }
 
+/*
+CPFIsValid check if the CPF is valid
+  - @param {string} doc - CPF
+  - @return {error} - error
+*/
 func CPFIsValid(doc string) error {
 	return cpf.IsValid(doc)
 }
 
+/*
+CPFFormat format the a valid CPF
+  - @param {string} doc - CPF
+  - @return {string} - CPF formatted
+  - @return {error} - error
+*/
 func CPFFormat(doc string) (string, error) {
 	return cpf.Format(doc)
 }
 
+/*
+CPFGenerate generate a valid CPF
+  - @return {string} - CPF
+  - @return {error} - error
+*/
 func CPFGenerate() (string, error) {
 	return cpf.Generate()
 }
 
+/*
+CNHIsValid check if the CNH is valid
+  - @param {string} doc
+  - @return {error} - error
+*/
 func CNHIsValid(doc string) error {
 	return cnh.IsValid(doc)
 }
 
+/*
+CNHGenerate generate a valid CNH
+  - @return {string} - CNH
+  - @return {error} - error
+*/
 func CNHGenerate() (string, error) {
 	return cnh.Generate()
 }
 
+/*
+VoterRegistrationIsValid check if the Voter Registration is valid
+  - @param {string} doc - Voter Registration
+  - @return {error} - error
+*/
 func VoterRegistrationIsValid(doc string) error {
 	return voterRegistration.IsValid(doc)
 }
 
+/*
+VoterRegistrationFormat format the a valid Voter Registration
+  - @param {string} doc - Voter Registration
+  - @return {string} - Voter Registration formatted
+  - @return {error} - error
+*/
 func VoterRegistrationFormat(doc string) (string, error) {
 	return voterRegistration.Format(doc)
 }
 
+/*
+VoterRegistrationGenerate generate a valid Voter Registration
+  - @param {string} uf - UF is the state of Brazil or ZZ for foreigners
+  - @return {string}
+  - @return {error}
+*/
 func VoterRegistrationGenerate(uf string) (string, error) {
 	return voterRegistration.Generate(uf)
 }
